bdf6c45b615bb53432ac491a84889b6cd38d0747
hansss12 <raihan.aqil2003@gmail.com>
2023-07-24 18:33:36 +0700

    feat: Add Liked Songs To header

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Sidebar>
-          {children}
+        {children}
         </Sidebar>
       </body>
     </html>
