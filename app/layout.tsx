09112547a15a1407a3a3a82cd4d5610866c15fdf
hansss12 <raihan.aqil2003@gmail.com>
2023-08-04 17:22:06 +0700

    fix: bug in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import SupabaseProvider from '@/providers/SupabaseProvider'
-import UserProvider from '@/providers/Userprovider'
+import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 
 const font = Figtree({ subsets: ['latin'] })
